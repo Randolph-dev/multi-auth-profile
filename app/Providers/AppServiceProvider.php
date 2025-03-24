410929a3a9edf316ab8675c88d60db8df746a557
Randolph-dev <randolph2894@gmail.com>
2025-03-24 13:46:01 +0200

    register filesystem singleton in AppServiceProvider and remove SocialiteServiceProvider from config

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,6 +13,9 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->singleton('files', function ($app) {
+            return new \Illuminate\Filesystem\Filesystem();
+        });
     }
 
     /**
